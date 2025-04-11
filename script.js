76aa806d5d0d6304c1e7acaa63e6fb894af8787d
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-11 21:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+let btn=document.querySelector("#add");
+let table=document.querySelector("table");
+let h3=document.querySelector("#total");
+let count=0;
+btn.addEventListener("click",()=>{
+	
+	let nameBox=document.querySelector("#item-name-input").value;
+    let priceBox=document.querySelector("#item-price-input").value;
+	let tr=document.createElement("tr");
+	let td1=document.createElement("td");
+	td1.innerText=nameBox;
+	let td2=document.createElement("td");
+	td2.innerText=priceBox;
+	count+=priceBox;
+	h3.innerText=count;
+	
+
+	tr.append(td1,td2);
+	table.append(tr);
+})
+
